403daafdca2562b3eef28a920fd6e166be520cd1
volodpj <volod.af@gmail.com>
2019-10-27 22:10:24 +0200

    Updates

---
+++ precache-manifest.7257c7f3cfb8e465d8519ed3a548ec1e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0ab1ae69ad623887aca19f1f978b7d0",
+    "url": "/testSoftBlues/index.html"
+  },
+  {
+    "revision": "49a54073eb15104a3ded",
+    "url": "/testSoftBlues/static/css/main.f05016c4.chunk.css"
+  },
+  {
+    "revision": "05b4adb84f097fadd209",
+    "url": "/testSoftBlues/static/js/2.7fb3da72.chunk.js"
+  },
+  {
+    "revision": "49a54073eb15104a3ded",
+    "url": "/testSoftBlues/static/js/main.db962cc8.chunk.js"
+  },
+  {
+    "revision": "8fb7aa491bf17a0f5a04",
+    "url": "/testSoftBlues/static/js/runtime-main.7c54df40.js"
+  }
+]);
---
+++ static/js/main.db962cc8.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonptest-soft-blues"]=this["webpackJsonptest-soft-blues"]||[]).push([[0],{3:function(t,e,n){t.exports=n(9)},8:function(t,e,n){},9:function(t,e,n){"use strict";n.r(e);var s=n(0),a=n.n(s),r=n(2),c=n.n(r);n(8);var o=function(){return a.a.createElement("div",{className:"App"},a.a.createElement("h1",null,"ttt"))};c.a.render(a.a.createElement(o,null),document.getElementById("root"))}},[[3,1,2]]]);
+//# sourceMappingURL=main.db962cc8.chunk.js.map

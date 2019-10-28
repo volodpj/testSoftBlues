3b39a195abbf431e955b21929dfeb12ee7f8acc3
volodpj <volod.af@gmail.com>
2019-10-28 15:38:18 +0200

    head

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
 import React from 'react';
-
+import './reset.css';
 import './App.css';
+import logo from './img/Logo.png';
 
 function App() {
   return (
     <div className="App">
-      <h1>ttt</h1>
+      <div className="wrapper">
+        <header className="header">
+          <img src={logo}/>
+          <nav className="header__navigation">
+            <a href="#">О компании</a>
+            <a href="#">Услуги</a>
+            <a href="#">Отзывы</a>
+            <a href="#">Контакти</a>
+          </nav>
+        </header>
+        <main className="mainContent">
+          <section className="mainContent__head">
+    
+          </section>
+        </main>
+      </div>
     </div>
   );
 }
